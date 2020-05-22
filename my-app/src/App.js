a8c50fd40ef3b8bdd815e383cd535272abf8aee2
Malhar Dave <malhar197@Malhars-MacBook-Air.local>
2020-05-22 14:15:37 -0400

    image functionality added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Post from './components/Post';
+import image from './image.jpg';
 
 function App() {
   return (
     <div className="App">
-      <div className= "Post">
+      <div>
       <Post content = "Insert clever comment" />
+      </div>
+      <div>
+      <Post content = {image} />
       </div>
     </div>
   );
--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -1,11 +1,21 @@
 import React from "react";
+import '../image.jpg';
+
+const regex = RegExp('[.]jpg$');
 
 const Post = props => {
+	if (!regex.test(props.content)){
 	return (
-		<div>
+		<div className= "Post">
 		<h2>{props.content}</h2>
 		</div>
 		);
+} else{
+	return (
+		<div className = "Pic">
+		<img src = {props.content} alt = 'something went wrong!' />
+		</div> );
+}
 };
 
 export default Post;
