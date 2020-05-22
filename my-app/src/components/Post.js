79956998b9eb4171e45def0fba7a0bd325ab416f
Malhar Dave <malhar197@Malhars-MacBook-Air.local>
2020-05-22 16:32:46 -0400

    css added

--- my-app/src/components/Post.js
+++ my-app/src/components/Post.js
@@ -6,15 +6,27 @@ const regex = RegExp('[.]jpg$');
 const Post = props => {
 	if (!regex.test(props.content)){
 	return (
+		<div>
 		<div className= "Post">
 		<h2>{props.content}</h2>
+		</div>
+		<div>
+		<br />
+		<br />
+		</div>
 		</div>
 		);
 } else{
 	return (
+		<div>
 		<div className = "Pic">
 		<img src = {props.content} alt = 'something went wrong!' />
-		</div> );
+		</div>
+		<div>
+		<br />
+		<br />
+		</div>
+		</div>);
 }
 };
 
