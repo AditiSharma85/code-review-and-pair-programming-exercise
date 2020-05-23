301ba12d9c66723d8412ed48a5f27b2d5265f5bb
Malhar Dave <malhar197@Malhars-MacBook-Air.local>
2020-05-23 04:40:00 -0400

    CommentList test added

--- my-app/src/tests/Post.test.js
+++ my-app/src/tests/Post.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Post from "../components/Post";
 import LikeButton from "../components/LikeButton";
+import CommentList from "../components/CommentList";
 import { render,screen,fireEvent } from "@testing-library/react";
-import { getByText,createEvent } from "@testing-library/dom";
+import { getByText,getByLabelText,createEvent } from "@testing-library/dom";
 
 // Testing for Post component. 
 // Post with some content is rendered and then, using getByText, 
@@ -15,6 +16,8 @@ test("Post component receives props and renders text", () => {
 
 	expect.stringContaining("Give me all your likes!");	
 });
+
+//Testing for LikeButton component.
 //Post is rendered, alongwith LikeButton.
 //LikeButton is then clicked, and string containing 
 //increased like count is expected.
@@ -29,4 +32,24 @@ test("Like component is clicked, and counter is increased", () => {
 	fireEvent.click(button);
 
 	expect.stringContaining("Likes: 1");
+});
+
+//Testing for CommentList component.
+//Post is rendered, and Add Comment label is selected.
+//The string 'abc' is passed, and expected.
+test("String is typed in Comment component and submit button clicked, string is displayed on screen", () => {
+
+	render(
+		<Post content = "What were the first three letters of the alphabet again?" />
+		);
+
+	const input = screen.getByLabelText('Add Comment:');
+
+	fireEvent.keyDown(input, {key: 'a', code: 'keyA'});
+	fireEvent.keyDown(input, {key: 'b', code: 'keyB'});
+	fireEvent.keyDown(input, {key: 'c', code: 'keyC'});
+	fireEvent.click(screen.getByText('Post'));
+
+	expect.stringContaining("abc");
+
 });
