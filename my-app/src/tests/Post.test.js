42424947dc30e745c6a5448a75972c6a68fa374e
Malhar Dave <malhar197@Malhars-MacBook-Air.local>
2020-05-23 02:41:14 -0400

    comment added for post TDD

--- my-app/src/tests/Post.test.js
+++ my-app/src/tests/Post.test.js
@@ -3,6 +3,7 @@ import Post from "../components/Post";
 import { render,screen } from "@testing-library/react";
 import { getByText } from "@testing-library/dom";
 
+//Testing for Post component. Post with some content is rendered and then, using getByText, the String is searched on react testing library screen.
 test("Post component receives props and renders text", () => {
 
 	render(
