4707f4c80293d3972865e70ba9c1d1deac7968f0
Malhar Dave <malhar197@Malhars-MacBook-Air.local>
2020-05-23 03:59:43 -0400

    post tdd modified

--- my-app/src/tests/Post.test.js
+++ my-app/src/tests/Post.test.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Post from "../components/Post";
-import { render,screen } from "@testing-library/react";
-import { getByText } from "@testing-library/dom";
+import LikeButton from "../components/LikeButton";
+import { render,screen,fireEvent } from "@testing-library/react";
+import { getByText,createEvent } from "@testing-library/dom";
 
-//Testing for Post component. Post with some content is rendered and then, using getByText, the String is searched on react testing library screen.
+// Testing for Post component. 
+// Post with some content is rendered and then, using getByText, 
+// the String is searched on react testing library screen.
 test("Post component receives props and renders text", () => {
 
 	render(
 		<Post content = "Give me all your likes!" />
 	);
 
-	const testResult = screen.getByText("Give me all your likes!");	
+	expect.stringContaining("Give me all your likes!");	
 });
